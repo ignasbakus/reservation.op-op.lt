5f09ac26faaf4d91fae683f1ed4d982eaa5c62c9
ignasbakus <ignas.bakus77@gmail.com>
2024-07-31 11:13:28 +0300

    bug email 1.4

--- resources/views/mail/user/payments/payment_received.blade.php
+++ resources/views/mail/user/payments/payment_received.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Batuto Nuomos Patvirtinimas</title>
     <link href="/frameworks/bootstrap-5.3.3/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-    <link href="/frameworks/bootstrap-icons-1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
 </head>
 <body>
 <div style="max-width: 600px; margin: 0 auto;">
@@ -104,13 +104,13 @@
 
     <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; font-size: 12px; color: black; background-color: #B6D2F7; max-width: 650px; min-height: 150px; margin: 0 auto; padding: 20px; text-align: center;">
         <a href="ADDLINKTOFACEBOOK" style="margin-bottom: 10px; text-decoration: none; color: black;">
-            <svg style="margin-bottom: 10px;" width="20" height="20" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" style="margin-bottom: 10px;" width="20" height="20" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
                 <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"></path>
             </svg>
         </a>
         <a href="configas" style="font-size: 16px;">op-op.lt</a>
         <a href="{{config('contactInfo.phone')}}" style="font-size: 18px; font-weight: 500; text-decoration: none; color: black;">
-            <svg width="20" height="20" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
                 <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"></path>
             </svg>
             {{config('contactInfo.phone')}}</a>
